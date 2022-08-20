951c8e518984c76d02257dd482c7ffbf9b2b2015
I.M <beniiorga@gmail.com>
2022-08-20 18:34:25 +0200

    testing changes to main view

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,19 @@
 <script lang="ts" setup>
 import { RouterView } from "vue-router";
-
 import { ref } from "vue";
 
-import Sidebar from "primevue/sidebar";
-import Button from "primevue/button";
-
 import NavigationMenu from "@/components/navigationMenu.vue";
-
-const visibleLeft = ref(true);
-const expanded = ref(false);
-const log = console.log;
 </script>
 
 <template>
   <template v-if="$route.name">
-    <div class="container">
-      <!--      <Sidebar-->
-      <!--        id="sidebar"-->
-      <!--        ref="sidebar"-->
-      <!--        v-model:visible="visibleLeft"-->
-      <!--        :class="{-->
-      <!--          'p-sidebar-sm': !visibleLeft.value,-->
-      <!--          'p-sidebar-md': visibleLeft.value,-->
-      <!--        }"-->
-      <!--        :dismissable="true"-->
-      <!--        :modal="true"-->
-      <!--      >-->
-      <!--        <Button-->
-      <!--          class="w-10 h-10"-->
-      <!--          icon="pi pi-arrow-right"-->
-      <!--          @click="-->
-      <!--            log(expanded);-->
-      <!--            expanded = !expanded;-->
-      <!--          "-->
-      <!--        />-->
-      <!--      </Sidebar>-->
-      <NavigationMenu />
-    </div>
+    <NavigationMenu />
   </template>
-  <div class="container">
-    <RouterView />
-  </div>
+  <RouterView />
 </template>
 
 <script lang="ts"></script>
 
 <style>
 @import "@/assets/base.css";
-
-.p-sidebar {
-  background-color: blue;
-}
-
-.p-sidebar-left.p-sidebar-sm {
-  width: 3rem;
-}
-
-.p-sidebar-left.p-sidebar-md {
-  width: 20rem;
-}
 </style>
